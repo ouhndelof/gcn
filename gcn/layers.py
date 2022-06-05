842f0d507898a6d65a8c09f67eaf8d5ecde988ed
tobiaskn <tobias.netskar@gmail.com>
2022-06-05 15:26:17 +0200

    Replace tf.app.flags with tf.compat.v1.flags

--- gcn/layers.py
+++ gcn/layers.py
@@ -1,7 +1,7 @@
 from gcn.inits import *
 import tensorflow as tf
 
-flags = tf.app.flags
+flags = tf.compat.v1.flags
 FLAGS = flags.FLAGS
 
 # global unique layer ID dictionary for layer name assignment
